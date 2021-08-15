b04041c60c9ede11b2750d782228439b19ff1965
Karen Gonzalez <57917529+kvaleriagonzalez@users.noreply.github.com>
2021-08-15 18:53:29 -0400

    adds models image and routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,48 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const User = require('../api/models/User');
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
 
-app.listen("500", () => {
+const uri = "mongodb://localhost:27017/blog";
+
+app.use(express.json());
+
+
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log(`Successfully connected to: ${uri}`);
+  })
+  .catch((err) => console.log(err.message));
+
+  const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
+// test if localhost:4000 is connected
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
+
+
+app.listen("3000", () => {
     console.log("back end is running");
 });
+
